28fa659a341efbfe378b925caaf13fceea118cec
Nandish-d3v <nandish@d3vtech.com>
2024-11-15 09:05:15 +0530

    Fixed the registration form UI and added validation

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,7 +16,7 @@
 
         <!-- Name -->
         <div>
-            <label for="name">Name:</label>
+            <label for="username">Username:</label>  <!-- Changed from 'name' to 'username' -->
             <input id="name" type="text" name="name" value="{{ old('name') }}" required autofocus>
             @error('name')
                 <div>{{ $message }}</div>
